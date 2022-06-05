dfebdb5cda44df9d47024e9f96c02bb01c1b5e9e
ShwetaPansare30 <Shwetambaripansare30@gmail.com>
2022-06-05 17:16:36 +0530

    3rd commit

---
+++ src/Test/LoginTest.java
@@ -0,0 +1,15 @@
+package Test;
+
+import org.testng.annotations.Test;
+
+public class LoginTest {
+
+	@Test
+	public static void TestCase(){
+		System.out.println("LoginTest");
+		
+		
+		
+		
+	}
+}
